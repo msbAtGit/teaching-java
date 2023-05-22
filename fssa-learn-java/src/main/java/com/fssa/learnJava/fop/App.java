51d96d0e655abc020dce21850bef158d0507ca0c
Bharathwaj Soundararajan <bsoundararajan@ctr.freshworks.com>
2023-05-22 12:19:07 +0530

    Pushing the directory structure for the project

---
+++ fssa-learn-java/src/main/java/com/fssa/learnJava/fop/App.java
@@ -0,0 +1,13 @@
+package com.fssa.learnJava.fop;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ fssa-learn-java/src/main/java/com/fssa/learnJava/fop/day01/HelloWorld.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package com.fssa.learnJava.fop.day01;
+
+/**
+ * @author BharathwajSoundarara
+ *
+ */
+public class HelloWorld {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello World");
+		System.out.println("Weclome to Java Programming");
+	}
+}
