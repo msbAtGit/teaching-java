e394d5e32727c7b02cb9b834493930e3348e2e51
Bharathwaj Soundararajan <bsoundararajan@ctr.freshworks.com>
2023-05-29 14:58:18 +0530

    Adding day 07 code for conditionals

---
+++ fssa-learn-java/src/main/java/com/fssa/learnJava/fop/day07/ConditionalStatementDemo.java
@@ -0,0 +1,22 @@
+/**
+ * 
+ */
+package com.fssa.learnJava.fop.day07;
+
+/**
+ * @author BharathwajSoundarara
+ *
+ */
+public class ConditionalStatementDemo {
+
+	public static void main(String[] args) {
+		
+	    int marks = 85;
+    
+      if ( marks >= 75 ) {
+          System.out.println("You got an A grade");
+      }
+      
+	}
+
+}
---
+++ fssa-learn-java/src/main/java/com/fssa/learnJava/fop/day07/ConditionalStatementDemo3.java
@@ -0,0 +1,21 @@
+package com.fssa.learnJava.fop.day07;
+
+public class ConditionalStatementDemo3 {
+
+	public static void main(String[] args) {
+
+		int mark = 70;
+
+		if (mark >= 75) {
+			System.out.println("You got an A grade");
+		} else if(mark >= 60) { 
+			System.out.println("You got an B grade");
+		} else if(mark >= 45) { 
+			System.out.println("You got an C grade");
+		} else {
+			System.out.println("You didn't get an A, B or C grade");
+		}
+
+	}
+
+}
