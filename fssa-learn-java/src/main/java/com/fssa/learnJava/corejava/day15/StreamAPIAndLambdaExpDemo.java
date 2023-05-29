20accfb740829c414c341cebca35f8c5976e9b13
Bharathwaj Soundararajan <bsoundararajan@ctr.freshworks.com>
2023-05-29 15:29:51 +0530

    Adding array solved example codes

---
+++ fssa-learn-java/src/main/java/com/fssa/learnJava/corejava/day15/StreamAPIAndLambdaExpDemo.java
@@ -0,0 +1,48 @@
+/**
+ * 
+ */
+package com.fssa.learnJava.corejava.day15;
+
+import java.util.Arrays;
+import java.util.stream.IntStream;
+
+public class StreamAPIAndLambdaExpDemo {
+
+	public static void main(String[] args) {
+
+	
+		// Array with a length of 3
+		int[] marks = new int[3];
+		
+		// Another way to declare array
+		int[] marks2 = {90, 10, 33};
+		
+		// assign marks in the array
+		marks[0] = 50;
+		marks[1] = 60;
+		marks[2] = 70;
+		
+	
+
+    
+		// find how many items in the array
+    System.out.println("Array size is: " + marks.length);
+		
+	
+		// Iterate array using for loop
+		for (int i = 0; i < marks.length; i++) {
+			int m = marks[i];
+			System.out.println("Mark is: " + m);
+		}
+		
+//		Lambda expression HOTS
+		IntStream arryAsStream =  Arrays.stream(marks2);
+		
+		arryAsStream.forEach((x) -> System.out.println("Marks: " + x));
+	}
+	
+	
+	
+
+}
+
---
+++ fssa-learn-java/src/main/java/com/fssa/learnJava/fop/day09/StringArrayDemo.java
@@ -0,0 +1,33 @@
+/**
+ * 
+ */
+package com.fssa.learnJava.fop.day09;
+
+/**
+ * @author BharathwajSoundarara
+ *
+ */
+public class StringArrayDemo {
+
+	public static void main(String[] args) {
+
+		// Array with a length of 3
+		String[] names = new String[3];
+
+		// assign marks in the array
+		names[0] = "Surya";
+		names[1] = "Vijay";
+		names[2] = "Ajith";
+
+    // find how many items in the array
+    System.out.println("Array size is: " + names.length);
+		
+		// Iterate array using for loop
+		for (int i = 0; i < names.length; i++) {
+			String m = names[i];
+			System.out.println("Name is: " + m);
+		}
+
+	}
+
+}
