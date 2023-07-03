bb4b39e6a5b58d2ef85e50debe060b8fda9e1f83
Bharathwaj Soundararajan <bsoundararajan@ctr.freshworks.com>
2023-07-03 16:14:54 +0530

    Adding more examples for ArrayList

---
+++ fssa-learn-java/src/main/java/com/fssa/learnJava/corejava/day06/ConvertArraytoList.java
@@ -0,0 +1,20 @@
+package com.fssa.learnJava.corejava.day06;
+
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * Demo class to show how to convert an Array into a List
+ * @author BharathwajSoundarara
+ *
+ */
+public class ConvertArraytoList {
+	public static void main(String[] args) {
+		String[] deptNames = new String[3];
+		deptNames[0] = "CSE";
+		deptNames[1] = "EEE";
+		deptNames[2] = "MECH";
+		List<String> list = Arrays.asList(deptNames);
+		System.out.println(list);
+	}
+}
---
+++ fssa-learn-java/src/main/java/com/fssa/learnJava/corejava/day06/TestArrayList.java
@@ -0,0 +1,24 @@
+package com.fssa.learnJava.corejava.day06;
+
+import java.util.ArrayList;
+
+public class TestArrayList {
+	 
+    //without using generics
+    public static void main(String[] args) {
+     
+        // Store the city names in the List
+        ArrayList cityList  = new ArrayList();
+        cityList.add("Chennai");
+        cityList.add("Bangalore");
+        cityList.add("Mumbai");
+         
+        // Display the city names
+        for (Object object : cityList) {
+            String cityName = (String) object; // TypeCast object to String
+            System.out.println(cityName);           
+        }
+ 
+    }
+ 
+}
